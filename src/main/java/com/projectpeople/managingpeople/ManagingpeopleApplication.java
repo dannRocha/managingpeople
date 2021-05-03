c11931a0af9730a7192c9c35a374eb7903383d23
Daniel Rocha <rochadaniel15@gmail.com>
2021-05-03 19:59:07 -0300

    chore: Adicionado arquivos para o git

---
+++ src/main/java/com/projectpeople/managingpeople/ManagingpeopleApplication.java
@@ -0,0 +1,13 @@
+package com.projectpeople.managingpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingpeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingpeopleApplication.class, args);
+	}
+
+}
