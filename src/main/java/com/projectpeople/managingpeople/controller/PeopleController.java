f24d784b44ea394981110902cd169986d9af78b4
Daniel Rocha <rochadaniel15@gmail.com>
2021-05-03 20:51:04 -0300

    fix: Corrigido nome da classe PersonController para PeopleController

--- src/main/java/com/projectpeople/managingpeople/controller/PeopleController.java
+++ src/main/java/com/projectpeople/managingpeople/controller/PeopleController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
-public class PersonController {
+public class PeopleController {
 
   @GetMapping
   public List<String> getPeople() {
